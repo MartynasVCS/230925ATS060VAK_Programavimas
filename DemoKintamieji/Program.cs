4185c6494b5b66786a23dc46c11e48b7aa636c0b
MartynoSiela <m.satas@gmail.com>
2023-10-11 21:24:23 +0300

    Add project files.

---
+++ DemoKintamieji/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace DemoKintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args) // Komentaras
+        {
+            // Čia galite naudoti lietuviškas raides
+
+            /*
+             * Komentaras
+             * Per
+             * Kelias
+             * Eilutes
+             */
+
+            // Sveikieji
+            var amzius = 15;
+            int skaicius = 10;
+            int naujasSkaicius = amzius;
+
+            // Trupmeniniai
+            float ugis = 1.91f; // metrai
+            float ugisBeKablelio = 191f; //centimetrai
+            double ugisTikslesnis = 1.123456789;
+
+            // Simboliniai
+            char simbolis = '@';
+            string tekstas = "Šiandien yra spalio 11 diena";
+            string tuscias = "";
+
+            // Loginiai
+            bool arLyja = false;
+            bool arTamsu = true;
+
+            // Kintamųjų reikšmių keitimas
+            string naujasTekstas = "Labas";
+            naujasTekstas = "Ate";
+            naujasTekstas = "Viso gero";
+
+            // Kintamieji be reikšmių
+            int skaiciusBeReiksmesSveikasis; // 0
+            float skaiciusBeReiksmesTrupmeninis; // 0
+            char simbolisBeReiksmes; // ???
+            string tekstasBeReiksmes; // ???
+        }
+    }
+}
