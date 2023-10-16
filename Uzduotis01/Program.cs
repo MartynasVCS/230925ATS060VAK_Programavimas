8b0fe370b82482a1cc5d60aff1f3f6618cebb253
MartynasVCS <martynasvcs@gmail.com>
2023-10-16 20:44:58 +0300

    Start homework 1

---
+++ Uzduotis01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Uzduotis01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Parašykite programą, 
+             * kuri išvestų žodį “Labas” 
+             * ir jūsų vardą toje pačioje eilutėje.
+             */
+
+            Console.WriteLine($"Labas, Martynai");
+
+            /*
+             * Susikurkite kintamąjį žmogaus amžiui nusakyti,
+             * išveskite į ekraną tekstą su šiuo amžiumi: 
+             * “Įvestas amžius: {amžius}”. 
+             * Tarp {} nurodytas kintamasis, 
+             * kurio reikšmė toje vietoje turi būti įstatyta.
+             */
+
+            /*
+             * Susikurkite skaičiaus kintamąjį, 
+             * kurį išveskite penkis kartus toje pačioje eilutėje
+             * be tarpų tarp šių skaičių 
+             * (skaičius - 25, išvedimas - 2525252525).
+             */
+
+            /*
+             * Susikurkite skaičiaus kintamąjį, 
+             * kurį išveskite penkis kartus toje pačioje eilutėje 
+             * su tarpais tarp šių skaičių 
+             * (skaičius - 25, rezultatas - 25 25 25 25 25).
+             */
+        }
+    }
+}
