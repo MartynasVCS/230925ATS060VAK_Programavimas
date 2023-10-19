cc4ba7b360e0ac94ca0cc4cf4b54c3ce380e1b1f
MartynasVCS <martynasvcs@gmail.com>
2023-10-19 21:15:53 +0300

    Add demo code for methods

---
+++ DemoMetodai/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DemoMetodai
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lietuviškų raidžių rodymas konsolėje
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Pasisveikinimas();
+
+            Console.Write("Įveskite savo vardą: ");
+            string vardas = Console.ReadLine();
+            PasisveikinimasSuVardu(vardas);
+            PasisveikinimasSuVardu("Petras");
+            PasisveikinimasSuVardu("Martynai");
+            PasisveikinimasSuVarduIrPavarde("Antanas", "Antanauskas");
+
+            int skaicius1 = 7;
+            int skaicius2 = 5;
+            Console.Write($"Skaičių {skaicius1} ir {skaicius2} suma yra: ");
+            Console.WriteLine(Skaiciuotuvas.Sudetis(skaicius1, skaicius2));
+
+            Console.Write($"Skaičių {skaicius1} ir {skaicius2} dalmuo yra: ");
+            Console.WriteLine(Skaiciuotuvas.Dalyba(skaicius1, skaicius2));
+
+            Skaiciuotuvas.DalybaIsDviejuIkiNulio(5);
+        }
+
+        static void Pasisveikinimas()
+        {
+            Console.WriteLine("Labas, pasauli!");
+        }
+
+        static void PasisveikinimasSuVardu(string vardas)
+        {
+            Console.WriteLine($"Labas, {vardas}!");
+        }
+
+        static void PasisveikinimasSuVarduIrPavarde(string vardas, string pavarde)
+        {
+            Console.Write($"Labas žmogau kurio vardas yra {vardas}");
+            Console.WriteLine($" o pavardė yra {pavarde}!");
+        }
+    }
+}
---
+++ DemoMetodai/Skaiciuotuvas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DemoMetodai
+{
+    internal class Skaiciuotuvas
+    {
+        static internal int Sudetis(int skaicius1, int skaicius2)
+        {
+            return skaicius1 + skaicius2;
+        }
+
+        static internal double Dalyba(int skaicius1, int skaicius2)
+        {
+            return (double)skaicius1 / skaicius2;
+        }
+
+        // Rekursinis metodas
+        static internal void DalybaIsDviejuIkiNulio(double skaicius)
+        {
+            double rezultatas = skaicius / 2;
+            Console.WriteLine(rezultatas);
+            if (Math.Round(rezultatas, 3) != 0)
+            {
+                DalybaIsDviejuIkiNulio(rezultatas);
+            }
+        }
+    }
+}
